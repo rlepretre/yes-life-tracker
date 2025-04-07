3666851978fd38ebe8150cfac6d218cc087a0a7e
rlepretre <rlepretre@Romains-MacBook-Pro.local>
2025-04-07 23:37:44 +0200

    🌼 add DaisyUI

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
-import './app.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
-document.querySelector('#app').innerHTML = `
+import "./app.css";
+
+document.querySelector("#app").innerHTML = `
   <div class="bg-primary">
     <p>
-      Test
       Hello World Vite
     </p>
+    <button class="btn">Default</button>
   </div>
-`
-
+`;
