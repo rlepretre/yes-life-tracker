02bedfb682577f803ec6b864dcb9d828e30f8f89
Romain Leprêtre <romain.lepretre@me.com>
2025-04-07 21:12:13 +0200

    :muscle: let the yes life begin

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './app.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+document.querySelector('#app').innerHTML = `
+  <div class="bg-primary">
+    <p>
+      Test
+      Hello World Vite
+    </p>
+  </div>
+`
+
